4195fbfa9f4e612b46a35c1ed7e01701141c27ae
zhak55 <zhak55@ya.ru>
2013-05-30 00:44:13 +0400

    Update simple-ajax.js

--- simple-ajax.js
+++ simple-ajax.js
@@ -7,45 +7,44 @@
  */
  
 function _createRequest() {
-	if(window.XMLHttpRequest === undefined) {
-	  window.XMLHttpRequest = function() {
-		try {
-		  return new ActiveXObject("Msxml2.XMLHTTP.6.0");
-			 } catch( $_ ) {
-		try {
-			return new ActiveXObject("Msxml2.XMLHTTP.3.0"); 
-			} catch ( $__ ) { }
-		  }
-		}
-	}else {
-        try { 
-	        return new window.XMLHttpRequest(); 
-	     } catch( $___ ) {} 
+  if(window.XMLHttpRequest === undefined) {
+    window.XMLHttpRequest = function() {
+       try { return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+	} catch( $_ ) {
+	  try {
+    return new ActiveXObject("Msxml2.XMLHTTP.3.0"); 
+	} catch ( $__ ) { }
        }
-	 }; 
-  function encodeData(data) {
-	  if(!data) return '';
-	  var mass = [];
-	   for(var name in data) {
-		   if(!data.hasOwnProperty(name)) continue;
-		   if(typeof data[name] === 'function') continue;
-		   var value = data[name].toString();
-		   name = encodeURIComponent(name.replace("%20", "+"));
-		   value = encodeURIComponent(value.replace("%20", "+"));
-		     mass.push(name + "=" + value);
-	   }
-	        return mass.join('&'); 
+      }
+     }else {
+        try { 
+	  return new window.XMLHttpRequest(); 
+	 } catch( $___ ) {} 
+       }
+     }; 
+function encodeData(data) {
+  if(!data) return '';
+  var mass = [];
+   for(var name in data) {
+                if(!data.hasOwnProperty(name)) continue;
+                if(typeof data[name] === 'function') continue;
+    var value = data[name].toString();
+    name = encodeURIComponent(name.replace("%20", "+"));
+    value = encodeURIComponent(value.replace("%20", "+"));
+     mass.push(name + "=" + value);
+   }
+return mass.join('&'); 
   };	 
 var ajax = {
   get   :   function(url, callback) {
     var $ = _createRequest(); 
       $.open("GET", url);
       $.onreadystatechange = function() {
-		 if ($.readyState === 4 && $.status === 200) {
-           callback($.responseText);
-		 }; 
-	   }
-	 $.send(null);
+        if ($.readyState === 4 && $.status === 200) {
+          callback($.responseText);
+         }; 
+       }
+    $.send(null);
   },
   post  :  function(url, data, callback) {
 	  
