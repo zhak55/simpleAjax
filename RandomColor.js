cf3de4139af0a8545b81324936bc212e580f0aab
zhak55 <zhak55@ya.ru>
2013-07-06 06:53:28 -0700

    Create RandomColor.js

---
+++ RandomColor.js
@@ -0,0 +1,17 @@
+var randColor = {
+  rV  :  function(val) {
+		return Math.floor(Math.random() * val);
+	},
+	get  :  function() {
+		var r = this.rV(255).toString(16);
+			if(r.length < 2)
+				   r = "0" + r;
+		var g = this.rV(255).toString(16);
+			if(g.length < 2)
+				   g = "0" + g;
+		var b = this.rV(255).toString(16);
+		    if(b.length < 2)
+		    	   b = "0" + b;
+      return "#" + r + g + b;
+	}
+} 
