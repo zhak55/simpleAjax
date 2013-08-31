8eea5e05f59afe7d1238c46cd8ef410b1c63ab2f
zhak55 <zhak55@ya.ru>
2013-08-31 05:24:51 -0700

    Update some.js

--- some.js
+++ some.js
@@ -18,16 +18,15 @@
         formats  =  [  'audio/mpeg; codecs="mp3"' , 'audio/ogg; codecs="vorbis"' , 'audio/wav; codecs="1"' , 'audio/mp4; codecs="mp4a.40.2' ],
 		
        // define formats that browser supports
-       isSupported = (function() {
+       isSupported  =  (function() {
          for( var i = 0; i < formats.length; i++) {
           if(audioElement.canPlayType(formats[i]) == "") continue;
             arraySupported.push(formats[i]); }
            return arraySupported; })(),
 
        // try to define the current browser
-       browser = function() {
+       browser  =  function() {
          var ua = navigator.userAgent.toLowerCase();
-		 
          var webkit  =  /(webkit)[ \/]([\w.]+)/
              , opera = /(opera)(?:.*version)?[ \/]([\w.]+)/
              , msie  = /(msie) ([\w.]+)/
@@ -39,18 +38,33 @@
         || [];
                return { browser : matches[1] || "none", version : matches[2] || "0" };
         },
-        		
-	// convert time into normal style
+		
+		// convert time into normal style
        convertedTime  =  function( time ) {  
         var seconds = 0, minutes = 0;
              seconds = Math.floor( time );   minutes = Math.floor( sec / 60 );
              minutes = minutes >= 10 ? minutes : '0' + minutes;    
         seconds = Math.floor( seconds % 60 );
         seconds = seconds >= 10 ? seconds : '0' + seconds;    
-            return minutes + ':' + seconds; }
-	
+            return minutes + ':' + seconds; };
 
+var _smartPlayer = {
 
-
+      loadAudio  :  function( url ) {
+      if( !urlExtensions.test( url )) newError('url');
+      thisObject = smartPlayer;
+       if(audioSupported && arraySupported.length >= 1)  {
+         if(startUrl == null) {
+           thisObject.defVolume = 0.7;
+           thisObject.audioElement = new Audio( url );
+           thisObject.audioElement.load();
+           thisObject.audioElement.play();
+           startUrl = url;
+		 }
+	   }
+	 }
+}
+		  
+ window.smartPlayer = _smartPlayer;
 
 })( window );
