cc1f141349f9feaa8fde12e1098d38076ff10cb3
zhak55 <zhak55@ya.ru>
2013-09-01 10:40:03 -0700

    Update some.js

--- some.js
+++ some.js
@@ -44,7 +44,7 @@
        // convert time into normal style
        convertedTime  =  function( time ) {  
         var seconds = 0, minutes = 0;
-             seconds = Math.floor( time );   minutes = Math.floor( sec / 60 );
+             seconds = Math.floor( time );   minutes = Math.floor( seconds / 60 );
              minutes = minutes >= 10 ? minutes : '0' + minutes;    
         seconds = Math.floor( seconds % 60 );
         seconds = seconds >= 10 ? seconds : '0' + seconds;    
