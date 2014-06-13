fa013f30f2f019e6319d38a4b3bbbde493aa959c
Roman Zhak <zhak55@ya.ru>
2014-06-13 23:32:12 +0400

    Update regexp.js

--- regexp.js
+++ regexp.js
@@ -1,5 +1,6 @@
-var tags = /<(?:[^<>!]*)>/g;
-
+var tags = /<(?:[^<>!]*)>/g
+  , tagsWithIn = /(?:<[^<>!]*>)([^<>])+(?:<\/[a-zA-Z]+>)/g;
+  
 '<!doctype html>   \
   <title><</title> \
   <span class></span>'.match( tags ); // returns: ["<title>", "</title>", "<span class>", "</span>"]
