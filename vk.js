644fcf0999c934ac9a5a734de417bce6f3e33fde
Roman Zhak <zhak55@ya.ru>
2014-07-16 18:11:18 +0400

    Create vk.js

---
+++ vk.js
@@ -0,0 +1,11 @@
+VK.init(function() { 
+  var req = new vk();
+      req.users("search", {q: "Pavel Durov", fields: "city"}).filter({city: "Санкт-Петербург"}, null, {token: "title"}).res(function(){
+        console.log( this.data.found )     
+      })
+      
+  console.log( req )
+  }, function() { 
+
+
+}, vk.latest ); 
