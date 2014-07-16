11c9c8633d9ce9a348bcd91bf7b9eb4689f4575e
Roman Zhak <zhak55@ya.ru>
2014-07-16 18:29:34 +0400

    Update vk.js

--- vk.js
+++ vk.js
@@ -1,8 +1,8 @@
 VK.init(function() { 
   var req = new vk();
       req.users("search", {q: "Pavel Durov", fields: "city"})
-       .filter({city: "Санкт-Петербург"}, null, {token: "title"}).res(function(){
-       // you get the filtered data 
+       .filter({city: "Санкт-Петербург"}, null, {token: "title"}).res(function( data ){
+       // you get the filtered data
         console.log( this.data.found )     
       })
       
