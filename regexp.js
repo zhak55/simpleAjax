f395e90be7a4f0d4f79e63548643f3ec38fbad23
Roman Zhak <zhak55@ya.ru>
2014-08-20 17:14:53 +0400

    Delete regexp.js

--- regexp.js
+++
@@ -1,6 +0,0 @@
-var tags = /<(?:[^<>!]*)>/g
-  , tagsWithIn = /(?:<[^<>!]*>)([^<>])+(?:<\/[a-zA-Z]+>)/g;
-  
-'<!doctype html>   \
-  <title><</title> \
-  <span class></span>'.match( tags ); // returns: ["<title>", "</title>", "<span class>", "</span>"]
