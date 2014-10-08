3aa7c592acff589901a1fa1f50d931eef3ffb7ff
Roman Zhak <zhak55@ya.ru>
2014-10-08 22:25:01 +0400

    Update vk.js

--- vk.js
+++ vk.js
@@ -21,32 +21,98 @@ vk.off({"LocationChanged":  completed, "Scroll" : scl, "WindowFocus": focus});
 
 }, vk.latest ); 
 
-(function(){
- this["ev"] = {};
- var store  = {};
+;
+(function(global, $) {
 
+    var makeRequest = function(method, params) {
+        if (!VK.api) throw new Error("VK API is unavailable now");
+        var deferred = Q.defer();
+        VK.api(method, params || {}, callback);
 
-    ev.on = function( type , fn ) {
-      !store[type] && (store[type] = []);
-       store[type].push( fn );
+        function callback(data) {
+            if (data.response) deferred.resolve(data.response);
+            else deferred.reject(new Error(data.error));
+        };
+        return deferred.promise;
     };
- 
-   ev.trigger = function( type , args ) {
-    for( var i = 0; i < store[type].length; i++) {
-     if( typeof store[type][i] === 'function' ){
-       store[type][i].apply( this , args || [] );
-     } else {
-       throw new Error("Event is not defined");
-         }
-       }
-    };
-}());
 
-ev.on("go", function( a ) {
-    console.log( a );
-});
-ev.on("done", function( a ) {
-    console.log( a );
-});
-ev.trigger("go", ["Cool"]);
-ev.trigger("done", ["Nice"]);
+    // backbone model 
+
+    var $vkDocuments = Backbone.Model.extend({
+        defaults: function() {
+            return {
+                did: '',
+                title: 'empty',
+                size: '',
+                ext: 'none',
+                url: ''
+            }
+        }
+    });
+
+    // backbone collection 
+
+    var $docs = new(Backbone.Collection.extend({
+        model: $vkDocuments,
+        initialize: function() {
+            makeRequest('docs.get').then(function(res) {
+                $docs.add(res.shift() && res);
+            })
+        }
+    }));
+
+    // backbone doc view 
+
+    var $docsView = Backbone.View.extend({
+        tagName: "li",
+        className: 'item-doc',
+        initialize: function() {
+            this.model.bind('destroy', this.remove, this);
+        },
+        events: {
+            "dblclick": "clear"
+        },
+        template: _.template($('#item-template').html()),
+        render: function() {
+            this.$el.addClass(this.model.get('ext'))
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+        clear: function() {
+            var self = this;
+            if (confirm('Are you sure you want to delete: ' + this.model.get('title'))) makeRequest("docs.delete", {
+                owner_id: this.model.get('owner_id'),
+                doc_id: this.model.get('did')
+            }).then(function(data) {
+                self.model.destroy();
+            }, function(err) {
+                alert(err)
+            })
+        }
+    });
+
+    // backbone global view 
+
+    var $AppView = Backbone.View.extend({
+
+        el: $("#vkApp"),
+        initialize: function() {
+
+            $docs.bind('add', this.fill, this);
+            this.to = this.$el.children(".docs");
+
+        },
+
+        fill: function(doc) {
+            var view = new $docsView({
+                model: doc
+            })
+            this.to.append(view.render().el);
+        }
+    });
+
+    new $AppView;
+
+
+
+}(window, jQuery));
