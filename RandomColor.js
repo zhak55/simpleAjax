9d3b33c4da891ae07921d838f6ab2c9c0a3aedad
Roman Zhak <zhak55@ya.ru>
2015-02-06 13:31:09 +0300

    Update RandomColor.js

--- RandomColor.js
+++ RandomColor.js
@@ -6,14 +6,18 @@ var randColor = (function(){
     return Math.floor( Math.random() * $value );
   };
 
+  // private const 
+  
+  var COUNT_C = 3;
+  
   // private props 
 
-  var rgb = new Array(3);
+  var rgb = new Array( COUNT_C );
 
   // public methods  
 
   return function() {
-  	for( var i = 0; i < rgb.length; i++ ) {
+  	for( var i = 0; i < COUNT_C; i++ ) {
   	  rgb[i] = randomize(255).toString(16);
   	  if( rgb[i].length < 2 ) rgb[i] = "0" + rgb[i];	
   	};
