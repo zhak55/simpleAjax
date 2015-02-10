6cffb727e912d6988b5072e7982392f8aaa86a94
Roman Zhak <zhak55@ya.ru>
2015-02-10 22:46:21 +0300

    Update core.js

--- core.js
+++ core.js
@@ -8,6 +8,15 @@ HTMLGameEngine(function() {
  return {
   scene  : "Mario", // the main scene id
   width  : 700,
-  height : 500
+  height : 500,
+  audio  : function( register ) {
+    register({src : "../music"} ,
+     [
+      { name: "background", src: "bg.mp3"  } ,
+      { name: "jumping"   , src: "jump.mp3"} ,
+      { name: "runnig"    , src : "run.mp3"}
+     ]);
+   
+  }
  }
 });
