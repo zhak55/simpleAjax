89b279c53ad595baef545ca92e0613e7be8f2f28
Roman Zhak <zhak55@ya.ru>
2015-02-10 22:26:25 +0300

    Create core.js

---
+++ core.js
@@ -0,0 +1,13 @@
+
+ // class - repeated sprites( e.g.: enemies )
+ // id    - unique( e.g.: character  );
+ 
+ // first - init new 2D html game 
+ 
+HTMLGameEngine(function() {
+ return {
+  scene  : "Mario", // the main scene id
+  width  : 700,
+  height : 500
+ }
+});
