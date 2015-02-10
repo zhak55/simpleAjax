640ffa8303209e042fe34c672881a04359b9a8ff
Roman Zhak <zhak55@ya.ru>
2015-02-10 23:37:27 +0300

    Update core.js

--- core.js
+++ core.js
@@ -4,34 +4,34 @@
  
  // first - init new 2D html game 
  
-var $main = new HTMLGameEngine(function() {
- return {
-  name   : "SuperMario"
-  scene  : "Mario", // the main scene id
-  width  : 700,
-  height : 500,
-   audio  : function( register ) {
-    register({root : "../music"} , 
-    [
-      { 
-          name: "background", 
-          src: "bg.mp3"
-      } , { 
-          name: "jumping", 
-          src: "jump.mp3"
-      } , { 
-          name: "runnig", 
-          src : "run.mp3"
-       }
-     ]);
-   },
-  image   : function( register ) {
-    register({root: "../img", type: "png"}, [
-      { name: "Mario", src: "mario" }
-     ])
-    }
-  }
-}).startLoad(["image", "audio"]);
+window["$main"] = new HTMLGameEngine(function() {
+  return {
+     name   : "SuperMario"
+     scene  : "Mario", // the main scene id
+     width  : 700,
+     height : 500,
+       audio  : function( register ) {
+         register({root : "../music"} , 
+          [
+            { 
+               name: "background", 
+               src: "bg.mp3"
+             } , { 
+               name: "jumping", 
+               src: "jump.mp3"
+             } , { 
+               name: "runnig", 
+               src : "run.mp3"
+            }
+         ])
+      },
+          image   : function( register ) {
+            register({root: "../img", type: "png"}, [
+              { name: "Mario", src: "mario" }
+            ])
+          }
+        }
+      }).startLoad(["image", "audio"]);
 
 // get DOM utils
 var dom = $main.acquire("DOM");
